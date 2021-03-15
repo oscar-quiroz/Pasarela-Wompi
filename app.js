4bb325bbeff9d95ce4ffa2c89d291d6d2540f300
oscar-quiroz <kairox.rt@gmail.com>
2021-03-15 14:40:13 -0500

    cambio de referencias web checkpout

--- app.js
+++ app.js
@@ -7,26 +7,30 @@ const shoeBg = document.querySelector('.shoeBackground');
 let prevColor = "blue";
 let animationEnd = true;
 
-function changeSize(){
+
+let reference = document.getElementById('reference');
+reference.value = new Date().getTime();
+
+function changeSize() {
     sizes.forEach(size => size.classList.remove('active'));
     this.classList.add('active');
 }
 
-function changeColor(){
-    if(!animationEnd) return;
+function changeColor() {
+    if (!animationEnd) return;
     let primary = this.getAttribute('primary');
     let color = this.getAttribute('color');
     let shoe = document.querySelector(`.shoe[color="${color}"]`);
     let gradient = document.querySelector(`.gradient[color="${color}"]`);
     let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
 
-    if(color == prevColor) return;
+    if (color == prevColor) return;
 
     colors.forEach(c => c.classList.remove('active'));
     this.classList.add('active');
 
     document.documentElement.style.setProperty('--primary', primary);
-    
+
     shoes.forEach(s => s.classList.remove('show'));
     shoe.classList.add('show');
 
@@ -47,35 +51,13 @@ colors.forEach(c => c.addEventListener('click', changeColor));
 
 let x = window.matchMedia("(max-width: 1000px)");
 
-function changeHeight(){
-    if(x.matches){
+function changeHeight() {
+    if (x.matches) {
         let shoeHeight = shoes[0].offsetHeight;
         shoeBg.style.height = `${shoeHeight * 0.9}px`;
+    } else {
+        shoeBg.style.height = "475px";
     }
-else{
- shoeBg.style.height = "475px";
-}
 }
 changeHeight();
 window.addEventListener('resize', changeHeight);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
